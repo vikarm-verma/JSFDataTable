14b1c285c20b4b3351d298b98954e30f61ab6050
vikram <vikram_6785@yahoo.com>
2020-05-20 10:42:24 +0530

    first commit

---
+++ src/java/Product.java
@@ -0,0 +1,22 @@
+public class Product {  
+private final int id;    
+private final String name;    
+private final float price;    
+
+
+
+public Product(int id, String name, float price) {    
+this.id = id;    
+this.name = name;    
+this.price = price;    
+}  
+public int getId() {  
+return id;  
+}  
+public String getName() {  
+return name;  
+}  
+public float getPrice() {  
+return price;  
+}  
+}    
